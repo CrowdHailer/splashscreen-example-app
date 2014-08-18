132a3e2ef7a4376335b96c99d7a3190b4340c78e
Peter <peterhsaxton@gmail.com>
2014-08-18 15:03:29 +0100

    first page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 
 gulp.task('build', function () {
-    gulp.src(['app/*', '!app/resources/'], {base: 'app/'})
+    gulp.src(['app/config.xml', 'app/index.html'], {base: 'app/'})
         .pipe(gulp.dest('www'))
+});
+
+gulp.task('js', function () {
+    gulp.src('app/js/index.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('www'));
 });
