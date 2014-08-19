610efeb2103e139935e64ae1d352c60f15641288
Peter <peterhsaxton@gmail.com>
2014-08-19 22:56:40 +0100

    cleaned up files with phongap assets dependance

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
-var fs = require('fs');
-var libxmljs = require("libxmljs");
 
 
 gulp.task('build', function () {
@@ -18,23 +16,4 @@ gulp.task('js', function () {
 gulp.task('default', function () {
     gulp.watch('app/js/*', ['js']);
     gulp.watch('app/index.html', ['build'])
-});
-var xml2js = require('xml2js');
-
-gulp.task('splashes', function () {
-    var parser = new xml2js.Parser();
-    fs.readFile(__dirname + '/app/config.xml', function(err, data) {
-        parser.parseString(data, function (err, result) {
-            result.widget['gap:splash'].forEach(function(item){
-                console.log(item.$['gap:platform']);
-                console.log(item.$['width']);
-                console.log(item.$['height']);
-                console.log(item.$['src']);
-            });
-            console.log('Done');
-        });
-    });
-    // console.log('hello');
-    // var raw = fs.readFileSync(__dirname + '/app/config.xml');
-    // console.log(raw);
 });
