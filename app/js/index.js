62c4b2b1de9dd02cfa1f536ae32f59b13e013c84
Peter <peterhsaxton@gmail.com>
2014-08-20 09:24:43 +0100

    all alerts for dom and device ready

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,33 @@
+alert('first message of three');
+
 require('domready')(function () {
-    alert('ready nooooow');
+    alert('message number two');
 })
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+        alert('message number three');
+    }
+};
+
+app.initialize()
